56112bba8025fa5d9c1d467a16f89153d959333a
Andreas Gruenwald <andreas@grynwald.net>
2020-08-06 22:15:45 +0200

    WIP: Add 'ReadFilesFromGit' module
    Fix failing unit test

--- src/Extensions.Statiq.Git.Test/Internal/RemoteGitRepositoryTest.cs
+++ src/Extensions.Statiq.Git.Test/Internal/RemoteGitRepositoryTest.cs
@@ -13,7 +13,8 @@ namespace Grynwald.Extensions.Statiq.Git.Test.Internal
         [Test]
         public void Disposing_the_GitRepository_deletes_the_temporary_repository_directory()
         {
-            using var sut = new RemoteGitRepository(m_WorkingDirectory);
+            // create repository instance, but do NOT use a "using" statement, Dispose() is called explicitly below.
+            var sut = new RemoteGitRepository(m_WorkingDirectory);
 
             // Access branches to trigger cloning of the repository
             _ = sut.Branches;

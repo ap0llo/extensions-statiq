3a9428cc0a88d8d7ae5df12c4d06b5d695696008
Andreas Grünwald <andreas@grynwald.net>
2020-08-07 23:12:43 +0200

    WIP: Add "Document References" modules
    Add "ResolveDocumentReferences" module

---
+++ src/Extensions.Statiq.DocumentReferences/DocumentReferenceKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace Grynwald.Extensions.Statiq.DocumentReferences
+{
+    public static class DocumentReferenceKeys
+    {
+        public const string DocumentName = nameof(DocumentName);
+        public const string DocumentVersion = nameof(DocumentVersion);
+        public const string DocumentIdentity = nameof(DocumentIdentity);
+    }
+}

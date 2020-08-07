c8cc933fc9db788fc0f2c5d979189ca5437b29dc
Andreas Grünwald <andreas@grynwald.net>
2020-08-07 18:57:52 +0200

    WIP: Add "Document References" modules
    Add document reference resolver

---
+++ src/Extensions.Statiq.DocumentReferences/Model/DocsReferenceResolver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Grynwald.Utilities.Collections;
+
+namespace Grynwald.Extensions.Statiq.DocumentReferences.Model
+{
+    internal class DocumentReferenceResolver<TDocument> : IDocumentReferenceVisitor<TDocument?, DocumentIdentity?> where TDocument : class
+    {
+        private readonly ReversibleDictionary<DocumentIdentity, TDocument> m_Documents = new ReversibleDictionary<DocumentIdentity, TDocument>();
+
+
+        public TDocument? TryResolveDocument(DocumentReference reference, TDocument currentDocument)
+        {
+            if (reference is null)
+                throw new ArgumentNullException(nameof(reference));
+
+            if (currentDocument is null)
+                throw new ArgumentNullException(nameof(currentDocument));
+
+            var currentIdentity = TryGetIdentity(currentDocument);
+            return reference.Accept(this, currentIdentity);
+        }
+
+
+        public bool ContainsIdentity(DocumentIdentity identity) => m_Documents.ContainsKey(identity);
+
+        public void Add(DocumentIdentity identity, TDocument document) => m_Documents.Add(identity, document);
+
+        public TDocument? Visit(FullyQualifiedDocumentReference reference, DocumentIdentity? currentDocumentIdentity)
+        {
+            return m_Documents.GetValueOrDefault(reference.Identity);
+        }
+
+        public TDocument? Visit(SameNameDocumentReference reference, DocumentIdentity? currentDocumentIdentity)
+        {
+            if (currentDocumentIdentity is null)
+                return null;
+
+            var targetIdentity = currentDocumentIdentity.WithVersion(reference.Version);
+            return m_Documents.GetValueOrDefault(targetIdentity);
+        }
+
+        public TDocument? Visit(SelfDocumentReference reference, DocumentIdentity? currentDocumentIdentity)
+        {
+            if (currentDocumentIdentity is null)
+                return null;
+
+            return m_Documents.GetValueOrDefault(currentDocumentIdentity);
+        }
+
+        public TDocument? Visit(SameVersionDocumentReference reference, DocumentIdentity? currentDocumentIdentity)
+        {
+            if (currentDocumentIdentity is null)
+                return null;
+
+            var targetIdentity = currentDocumentIdentity.WithName(reference.Name);
+            return m_Documents.GetValueOrDefault(targetIdentity);
+        }
+
+
+        private DocumentIdentity? TryGetIdentity(TDocument document) => m_Documents.ReversedDictionary.GetValueOrDefault(document);
+    }
+}

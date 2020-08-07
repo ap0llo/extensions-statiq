f2ad7c608eb6047c997bab9bf11712435d1301a8
Andreas Gruenwald <andreas@grynwald.net>
2020-08-07 14:00:10 +0200

    feat: Add extensions methods to ease retrieval of git metadata.
    Add extension methods for 'IMetadata' to retrive git-metadata:
    'GetGitRepositoryUrl()', 'GetGitBranch()', 'GetGitCommit()' and 'GetGitRelativePath'

---
+++ src/Extensions.Statiq.Git/MetadataExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Statiq.Common;
+
+namespace Grynwald.Extensions.Statiq.Git
+{
+    public static class MetadataExtensions
+    {
+        /// <summary>
+        /// Gets the value for the <see cref="GitKeys.GitRepositoryUrl"/> key.
+        /// </summary>
+        public static string GetGitRepositoryUrl(this IMetadata metadata) => metadata.GetString(GitKeys.GitRepositoryUrl);
+
+        /// <summary>
+        /// Gets the value for the <see cref="GitKeys.GitBranch"/> key.
+        /// </summary>
+        public static string GetGitBranch(this IMetadata metadata) => metadata.GetString(GitKeys.GitBranch);
+
+        /// <summary>
+        /// Gets the value for the <see cref="GitKeys.GitCommit"/> key.
+        /// </summary>
+        public static string GetGitCommit(this IMetadata metadata) => metadata.GetString(GitKeys.GitCommit);
+
+        /// <summary>
+        /// Gets the value for the <see cref="GitKeys.GitRelativePath"/> key.
+        /// </summary>
+        public static string GetGitRelativePath(this IMetadata metadata) => metadata.GetString(GitKeys.GitRelativePath);
+    }
+}

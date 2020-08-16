88610dbc2914d6af5d1bc9bf2c22cc1a334d42db
Andreas Gruenwald <andreas@grynwald.net>
2020-08-16 20:00:40 +0200

    WIP: Add "docs" template
    Add projects for "docs template" (a template / theme aimed at documentation websites) and add pipeline to copy assets required by the theme to the output

---
+++ src/Extensions.Statiq.DocsTemplate/DocsTemplateKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace Grynwald.Extensions.Statiq.DocsTemplate
+{
+    public static class DocsTemplateKeys
+    {
+        public const string DocsTemplateThemeName = nameof(DocsTemplateThemeName);
+    }
+}
---
+++ src/Extensions.Statiq.DocsTemplate/DocsTemplateThemeNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Grynwald.Extensions.Statiq.DocsTemplate
+{
+    public static class DocsTemplateThemeNames
+    {
+        public const string Default = nameof(Default);
+        public const string Empty = nameof(Empty);
+    }
+}

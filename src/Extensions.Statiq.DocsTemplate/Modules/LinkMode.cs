adc3e1f0f5693bb6c2d6bf0cea4d26d514f959fa
Andreas Gruenwald <andreas@grynwald.net>
2020-08-22 17:12:29 +0200

    WIP: Add "docs" template
    Add "MarkNavbarItemsAsActive" module

---
+++ src/Extensions.Statiq.DocsTemplate/Modules/LinkMode.cs
@@ -0,0 +1,19 @@
+﻿namespace Grynwald.Extensions.Statiq.DocsTemplate.Modules
+{
+    /// <summary>
+    /// Enumerates option how links are resolved by the <see cref="MarkNavbarItemsAsActive"/> module.
+    /// </summary>
+    /// <seealso cref="ResolveDocumentReferences" />
+    public enum LinkMode
+    {
+        /// <summary>
+        /// Use the input documents' source path
+        /// </summary>
+        Destination = 0,
+
+        /// <summary>
+        /// Use the input documents' destination path
+        /// </summary>
+        Source = 1
+    }
+}

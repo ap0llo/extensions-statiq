5f6e86f51e8a4d5673d5550a4096bf164ab7a3db
Andreas Gruenwald <andreas@grynwald.net>
2020-08-22 17:31:51 +0200

    WIP: Add "docs" template
    Add "UseBootstrapTables" module

---
+++ src/Extensions.Statiq.DocsTemplate.Test/Modules/UseBootstrapTablesTest.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading.Tasks;
+using AngleSharp.Dom.Html;
+using FluentAssertions;
+using Grynwald.Extensions.Statiq.DocsTemplate.Modules;
+using NUnit.Framework;
+using Statiq.Common;
+using Statiq.Testing;
+
+namespace Grynwald.Extensions.Statiq.DocsTemplate.Test.Modules
+{
+    /// <summary>
+    /// Tests for <see cref="UseBootstrapTables"/>
+    /// </summary>
+    public class UseBootstrapTablesTest : BaseFixture
+    {
+        [Test]
+        public async Task Module_Adds_table_tag()
+        {
+            // ARRANGE
+            var document = new TestDocument(@"<html>
+                        <head>
+                            <title>Irrelevant</title>
+                        </head>
+                        <body>
+                            <table>
+                            </table>
+                        </body>
+                    </html>");
+
+            var sut = new UseBootstrapTables();
+
+            // ACT
+            var result = await ExecuteAsync(document, sut).SingleAsync();
+
+            // ASSERT
+            var html = await result.ParseAsHtmlAsync();
+
+            html.QuerySelectorAll("table")
+                .Should().ContainSingle()
+                .Which.Should().BeAssignableTo<IHtmlTableElement>()
+                .Which.ClassList
+                    .Should().ContainSingle()
+                    .And.Contain("table");
+        }
+
+        [Test]
+        [TestCase(BootstrapTableOptions.Bordered, "table-bordered")]
+        [TestCase(BootstrapTableOptions.Striped, "table-striped")]
+        [TestCase(BootstrapTableOptions.Small, "table-sm")]
+        public async Task Module_adds_specified_optional_classes(BootstrapTableOptions tableOptions, string expectedClass)
+        {
+            // ARRANGE
+            var document = new TestDocument(@"<html>
+                        <head>
+                            <title>Irrelevant</title>
+                        </head>
+                        <body>
+                            <table>
+                            </table>
+                        </body>
+                    </html>");
+
+            var sut = new UseBootstrapTables().WithTableOptions(tableOptions);
+
+            // ACT
+            var result = await ExecuteAsync(document, sut).SingleAsync();
+
+            // ASSERT
+            var html = await result.ParseAsHtmlAsync();
+
+            html.QuerySelectorAll("table")
+              .Should().ContainSingle()
+              .Which.Should().BeAssignableTo<IHtmlTableElement>()
+              .Which.ClassList
+                  .Should().HaveCount(2)
+                  .And.Contain(new[] { "table", expectedClass });
+        }
+    }
+}
---
+++ src/Extensions.Statiq.DocsTemplate/Modules/UseBootstrapTables.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AngleSharp.Dom.Html;
+using Statiq.Common;
+using Statiq.Html;
+
+namespace Grynwald.Extensions.Statiq.DocsTemplate.Modules
+{
+    /// <summary>
+    /// Adds Bootstrap table classes to all HTML table elements.
+    /// </summary>
+    /// <seealso href="https://getbootstrap.com/docs/4.5/content/tables/">Tables (Bootstrap Documentation)</seealso>
+    public sealed class UseBootstrapTables : Module
+    {
+        private BootstrapTableOptions m_TableOptions = BootstrapTableOptions.None;
+
+        /// <summary>
+        /// Set <see cref="BootstrapTableOptions"/> to use for processing tables
+        /// </summary>
+        public UseBootstrapTables WithTableOptions(BootstrapTableOptions tableOptions)
+        {
+            m_TableOptions = tableOptions;
+            return this;
+        }
+
+
+        protected override async Task<IEnumerable<IDocument>> ExecuteContextAsync(IExecutionContext context)
+        {
+            return await context.ExecuteModulesAsync(new ModuleList()
+            {
+                new ProcessHtml("table", element =>
+                {
+                    if(element is IHtmlTableElement tableElement)
+                    {
+                        tableElement.ClassList.Add("table");
+
+                        if(m_TableOptions.HasFlag(BootstrapTableOptions.Striped))
+                            tableElement.ClassList.Add("table-striped");
+
+                        if(m_TableOptions.HasFlag(BootstrapTableOptions.Bordered))
+                            tableElement.ClassList.Add("table-bordered");
+
+                        if(m_TableOptions.HasFlag(BootstrapTableOptions.Small))
+                            tableElement.ClassList.Add("table-sm");
+                    }
+                })
+            },
+            context.Inputs);
+        }
+    }
+}
